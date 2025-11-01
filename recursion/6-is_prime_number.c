8e7c8bedbdeaab7f70a263a92493cbf50cbf79fb
lilsouy <solaff.alessa@gmail.com>
2025-11-01 21:22:37 +0000

    new update

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+	* check_prime - helper function to check if number is prime
+	* @n: number to check
+	* @i: iterator
+	* Return: 1 if prime, 0 otherwise
+	*/
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+	return (0);
+	if (i * i > n)
+	return (1);
+	if (n % i == 0)
+	return (0);
+	return (check_prime(n, i + 1));
+}
+
+/**
+	* is_prime_number - checks if a number is prime
+	* @n: number to check
+	* Return: 1 if prime, 0 otherwise
+	*/
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
