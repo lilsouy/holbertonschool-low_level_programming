65051a9aa3f19a0a7ea60d0b1272ba39d9749c57
lilsouy <solaff.alessa@gmail.com>
2025-11-08 14:19:25 +0000

    new update

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+	* _calloc - allocates memory for an array and sets it to zero
+	* @nmemb: number of elements
+	* @size: size of each element
+	*
+	* Return: pointer to allocated memory, or NULL
+	*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+	ptr[i] = 0;
+
+	return (ptr);
+}
+
---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+	* array_range - creates an array of integers from min to max
+	* @min: minimum value
+	* @max: maximum value
+	*
+	* Return: pointer to the new array, or NULL
+	*/
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, size;
+
+	if (min > max)
+	return (NULL);
+
+	size = max - min + 1;
+
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+	return (NULL);
+
+	for (i = 0; i < size; i++)
+	arr[i] = min + i;
+
+	return (arr);
+}
+
