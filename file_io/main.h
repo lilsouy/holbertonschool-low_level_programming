8960460b9bb1c76b506a01e9b892cc32ca8d7736
lilsouy <solaff.alessa@gmail.com>
2025-12-01 15:25:34 +0000

    function that creates a file.

--- file_io/main.h
+++ file_io/main.h
@@ -10,6 +10,7 @@ int _putchar(char c);
 
 /* Prototype for the read_textfile function */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 
 #endif /* MAIN_H */
-
